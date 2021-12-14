94a5f38cb097bebbb0e77cb9c301f176955435be
Fernando Verri <fernandoverri80@gmail.com>
2021-12-14 14:13:07 -0300

    con path

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
 import React from "react";
 import "./App.css";
 import Reset from "./component/Reset";
+import Registro from "./component/Registro";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <Reset />
-      </header>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <header className="App-header">
+          <Routes>
+            <Route exact path={"/reset"} element={<Reset />} />
+            <Route exact path={"/registered"} element={<Registro />} />
+          </Routes>
+        </header>
+      </div>
+    </BrowserRouter>
   );
 }
 
---
+++ src/component/Registro.js
@@ -0,0 +1,27 @@
+const Registro = () => {
+  return (
+    <div
+      style={{
+        backgroundColor: "#474645",
+        width: "60%",
+        //height: "100%",
+        flexDirection: "row",
+        padding: 14,
+        alignItems: "center",
+        justifyContent: "space-between",
+      }}
+    >
+      <h1
+        style={{
+          color: "orangered",
+          justifyContent: "space-between",
+          alignItems: "center",
+        }}
+      >
+        Usuario Registrado
+      </h1>
+    </div>
+  );
+};
+
+export default Registro;
