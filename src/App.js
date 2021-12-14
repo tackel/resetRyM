a6e806132c50caffd63e1b676b320f477e6f9b75
Fernando Verri <fernandoverri80@gmail.com>
2021-12-14 14:37:20 -0300

    ultimo

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import "./App.css";
 import Reset from "./component/Reset";
 import Registro from "./component/Registro";
+import Home from "./component/Home";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
       <div className="App">
         <header className="App-header">
           <Routes>
+            <Route path="/" element={<Home />} />
             <Route exact path={"/reset"} element={<Reset />} />
             <Route exact path={"/registered"} element={<Registro />} />
           </Routes>
