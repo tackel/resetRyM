d0051b61d5f58c4aa0cd7e888f184c211fe20e3c
Fernando Verri <fernandoverri80@gmail.com>
2021-12-14 14:36:55 -0300

    ultimo

---
+++ src/component/Home.js
@@ -0,0 +1,29 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <div
+      style={{
+        backgroundColor: "#474645",
+        width: "60%",
+        //height: "100%",
+        flexDirection: "row",
+        padding: 14,
+        alignItems: "center",
+        justifyContent: "space-between",
+      }}
+    >
+      <h1
+        style={{
+          color: "orangered",
+          justifyContent: "space-between",
+          alignItems: "center",
+        }}
+      >
+        HOME
+      </h1>
+    </div>
+  );
+};
+
+export default Home;
